d673e94578a15da440cefd22020040b3b8c153b9
bilelexa <bilelexa>
2018-08-26 21:45:48 +0100

    Access-Control

--- src/main/java/com/exa/gestarticles/web/rest/GestArticlesRestAPI.java
+++ src/main/java/com/exa/gestarticles/web/rest/GestArticlesRestAPI.java
@@ -24,14 +24,32 @@ public class GestArticlesRestAPI extends AbstractVerticle{
 	@Override
     public void start() throws Exception {
 		
+		String defaultHeaders = "Origin, X-Requested-With, Content-Type, Accept"; 
+		String defaultMethods = "GET, POST, OPTIONS, PUT, HEAD, DELETE, CONNECT"; 
+		String defaultIpAndPorts = "*"; 
+		
 		Router router = Router.router(vertx);
+		
+		router.route().handler(rc -> {         
+		rc.response().putHeader("Access-Control-Allow-Headers", defaultHeaders); 
+		rc.response().putHeader("Access-Control-Allow-Methods", defaultMethods); 
+		rc.response().putHeader("Access-Control-Allow-Origin", defaultIpAndPorts); 
+		rc.response().putHeader("Access-Control-Allow-Credentials", "true"); 
+		rc.next(); 
+	     }); 
+		
 		router.route("/articles/*").handler(ResponseContentTypeHandler.create());
 		router.route(HttpMethod.POST, "/articles").handler(BodyHandler.create());
 		router.route(HttpMethod.PUT, "/articles").handler(BodyHandler.create());
+		router.route(HttpMethod.OPTIONS, "/articles/*").handler(rc -> { 
+			rc.response().setStatusCode(200);
+		    rc.response().end();
+		});
 		
 		router.get("/articles/:id").produces("application/json").handler(rc -> { 
 			Optional<Article> art = articleRepository.findById(Long.parseLong(rc.request().getParam("id")));
-		    rc.response().end(Json.encodePrettily(art.get()));
+			rc.response().setStatusCode(200);
+			rc.response().end(Json.encodePrettily(art.get()));
 		});
 		
 		router.get("/articles").produces("application/json").handler(rc -> { 
@@ -42,22 +60,25 @@ public class GestArticlesRestAPI extends AbstractVerticle{
 		router.post("/articles").produces("application/json").handler(rc -> { 
 			Article a = Json.decodeValue(rc.getBodyAsString(), Article.class);
 			articleRepository.save(a);
+			rc.response().setStatusCode(200);
 		    rc.response().end();
 		});
 		
 		router.put("/articles").produces("application/json").handler(rc -> { 
 			Article a = Json.decodeValue(rc.getBodyAsString(), Article.class);
 			articleRepository.save(a);
+			rc.response().setStatusCode(200);
 		    rc.response().end();
 		});
 		
 		router.delete("/articles/:id").handler(rc -> {
 			articleRepository.deleteById(Long.parseLong(rc.request().getParam("id")));
 		    rc.response().setStatusCode(200);
+		    rc.response().end();
 		});
 		
-		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-		System.out.println("HTTP server started on port 8080");
+		vertx.createHttpServer().requestHandler(router::accept).listen(3100);
+		System.out.println("HTTP server started on port 3100");
 	}
 
 }
